b6fe36918e86f7ff0d82a64942a797acb8dc52f4
Pnshimiye <pnshimiye@gmail.com>
2018-12-12 16:34:01 +0200

    Create files and migrate the first js codes

---
+++ Js/script.js
@@ -0,0 +1,12 @@
+var add= function( number1, number2) {
+  return number1 + number2;
+  }
+  var number1 =parseInt(prompt("enter number"));
+  var number2=parseInt(prompt("enter another number"))
+  alert(add(number1, number2))
+
+  var substract= function (number1, number2){
+    return number1 - number2
+    }
+    alert(substract(10,5))
+    
